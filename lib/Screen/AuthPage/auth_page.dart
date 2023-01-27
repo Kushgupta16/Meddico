7d93beb050c653365a842cd73b6f0767e7952347
Aditya Majumdar <90856119+ADITYAAAAAAAAAAAAA@users.noreply.github.com>
2023-01-28 01:42:50 +0530

    Login page changes

---
+++ lib/Screen/AuthPage/auth_page.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:meddico/Screen/LoginPage/LoginPage.dart';
+import 'package:meddico/Screen/bottomnav.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Bottomnav();
+          } else {
+            return LoginPage();
+          }
+        },
+      ),
+    );
+  }
+}
