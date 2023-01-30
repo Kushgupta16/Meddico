76519599fbf65a8f7966d3c71178f7cc195b7a6c
Aditya Majumdar <90856119+ADITYAAAAAAAAAAAAA@users.noreply.github.com>
2023-01-30 20:59:29 +0530

    Login page changes

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,11 +1,9 @@
-import 'dart:html';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
-  signInWithGoogle() async: {
-    final GoogleSignInAcccount? gUser = await GoogleSignIn().signIn();
+  signInWithGoogle() async {
+    final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
     final GoogleSignInAuthentication gAuth = await gUser!.authentication;
     final credential = GoogleAuthProvider.credential(
       accessToken: gAuth.accessToken,
