ccb109973c49f1aa2f7f9daddc94b3ac0ee12752
Aditya Majumdar <90856119+ADITYAAAAAAAAAAAAA@users.noreply.github.com>
2023-01-30 19:57:31 +0530

    Google auth changes

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  signInWithGoogle() async: {
+    final GoogleSignInAcccount? gUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: gAuth.accessToken,
+      idToken: gAuth.idToken,
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
