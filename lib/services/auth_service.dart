122a724e620235e69283bf2cba86c90181d19329
Aditya Majumdar <90856119+ADITYAAAAAAAAAAAAA@users.noreply.github.com>
2023-02-21 17:49:10 +0530

    Updates

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:meddico/Screen/bottomnav.dart';
 
 class AuthService {
   signInWithGoogle() async {
